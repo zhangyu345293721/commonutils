d68e5ae588c020b8d940dda1ec8e79572e4ab0e5
张瑀 <zhangyu_xtb@geekplus.cc>
2019-05-21 10:25:02 +0800

    添加解析xml的类

--- src/main/java/com/zhang/readjson/demo/ReadJson.java
+++ src/main/java/com/zhang/readjson/demo/ReadJson.java
@@ -22,7 +22,6 @@ public class ReadJson {
         System.out.println(person.toString());
     }
 
-
     /**
      * @Description: 文件读出成为字符串的形式
      * @Param: 无参数
@@ -30,13 +29,13 @@ public class ReadJson {
      * @Author: zhangyu
      */
     public String fileRead() throws Exception {
-        File file = new File("C:\\Users\\octopus\\Desktop\\person.json");//定义一个file对象，用来初始化FileReader
-        FileReader reader = new FileReader(file);//定义一个fileReader对象，用来初始化BufferedReader
-        BufferedReader bReader = new BufferedReader(reader);//new一个BufferedReader对象，将文件内容读取到缓存
-        StringBuilder sb = new StringBuilder();//定义一个字符串缓存，将字符串存放缓存中
+        File file = new File("C:\\Users\\octopus\\Desktop\\person.json");
+        FileReader reader = new FileReader(file);
+        BufferedReader bReader = new BufferedReader(reader);
+        StringBuilder sb = new StringBuilder();
         String s = "";
         while ((s = bReader.readLine()) != null) {//逐行读取文件内容，不读取换行符和末尾的空格
-            sb.append(s);//将读取的字符串添加换行符后累加存放在缓存中
+            sb.append(s);
             // System.out.println(s);
         }
         bReader.close();
