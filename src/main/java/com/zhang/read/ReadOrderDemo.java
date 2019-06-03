01ae33f665b03d2c5835e0ed734361ad8a7024ca
张瑀 <zhangyu_xtb@geekplus.cc>
2019-06-03 22:33:34 +0800

    添加读取一个文件夹下的所有文件的例子

---
+++ src/main/java/com/zhang/read/ReadOrderDemo.java
@@ -0,0 +1,33 @@
+package com.zhang.read;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: utilsdemo
+ * @description: 读取数据文件
+ * @author: zhangyu
+ * @create: 2019-05-30 13:29
+ */
+public class ReadOrderDemo {
+    @Test
+    public void fun() {
+        List<String> list = ReadFileToLine.getFileToList("C:\\Users\\octopus\\Desktop\\sql\\pick_order.csv");
+        List<String> smallList = new ArrayList<>();
+        for (int i = 0; i < 2000; i++) {
+            StringBuffer sb = new StringBuffer();
+            String[] strs = list.get(i).split("\\s+");
+            for (int j = 0; j < strs.length; j++) {
+                if (j == strs.length - 1) {
+                    sb.append(strs[j]);
+                } else {
+                    sb.append(strs[j] + ",");
+                }
+            }
+            smallList.add(sb.toString());
+        }
+        smallList.forEach(i -> System.out.println(i));
+    }
+}
