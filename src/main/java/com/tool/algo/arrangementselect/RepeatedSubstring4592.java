a323fd70c9d4487463c5a6948e02065e7fb131be
张瑀 <zhangyu_xtb@geekplus.cc>
2019-11-13 10:04:42 +0800

    增加1071,1189,459题目

--- src/main/java/com/tool/algo/arrangementselect/RepeatedSubstring4592.java
+++ src/main/java/com/tool/algo/arrangementselect/RepeatedSubstring4592.java
@@ -13,15 +13,19 @@ import java.util.stream.Collectors;
  * @author: zhangyu
  */
 public class RepeatedSubstring4592 {
-
-
     @Test
     public void repeatedSubstring() {
-        String s = "bb";
+        String s = "abcabcabc";
         boolean flag = repeatedSubstringPattern(s);
         System.out.println("flag = " + flag);
     }
 
+    /**
+     * 判断是否是重复子字符串
+     *
+     * @param s 主字符串
+     * @return 返回布尔值
+     */
     public boolean repeatedSubstringPattern(String s) {
         List<String> subStrList = getSubStrList(s);
         boolean flag = false;
@@ -33,12 +37,25 @@ public class RepeatedSubstring4592 {
         return flag;
     }
 
+    /**
+     * 判断字符串是否被整除
+     *
+     * @param str 分割字符串
+     * @param s   主字符串
+     * @return 返回布尔值
+     */
     private boolean isDivide(String str, String s) {
         String[] strings = s.split(str);
         String result = Arrays.asList(strings).stream().collect(Collectors.joining(""));
         return result.isEmpty();
     }
 
+    /**
+     * 获取所有的子串
+     *
+     * @param s 主字符串
+     * @return 返回所有的字符串
+     */
     private List<String> getSubStrList(String s) {
         List<String> list = new ArrayList<>();
         for (int i = 1; i < s.length(); i++) {
