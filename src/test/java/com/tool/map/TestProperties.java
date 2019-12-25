ac1fcc1c48a427318dd8e7dec8eb407061206eb8
张瑀 <zhangyu_xtb@geekplus.cc>
2019-12-25 11:29:25 +0800

    master:stream修改关闭流操作

--- src/test/java/com/tool/map/TestProperties.java
+++ src/test/java/com/tool/map/TestProperties.java
@@ -14,27 +14,17 @@ import java.util.Properties;
 public class TestProperties {
     public void writeProperties() {
         Properties properties = new Properties();
-        OutputStream output = null;
-        try {
-            output = new FileOutputStream("config.properties");
+        try (OutputStream output = new FileOutputStream("config.properties");) {
             properties.setProperty("url", "jdbc:mysql://localhost:3306/");
             properties.setProperty("username", "root");
             properties.setProperty("password", "root");
             properties.setProperty("databases", "music_player");
-            properties.store(output, "Steven1997 modify" + new Date().toString());
+            properties.store(output, "zhangyu modify" + new Date().toString());
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if(output!=null) {
-                try {
-                    output.close();
-                }catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
     }
+
     public static void main(String[] args) {
         TestProperties t = new TestProperties();
         t.writeProperties();
