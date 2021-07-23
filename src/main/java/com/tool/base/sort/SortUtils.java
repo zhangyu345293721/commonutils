d718332d88edeeaee8015b4d8201d575fa4ee43f
octopus <345293721@qq.com>
2021-07-23 12:45:45 +0800

    Update SortUtils.java

--- src/main/java/com/tool/base/sort/SortUtils.java
+++ src/main/java/com/tool/base/sort/SortUtils.java
@@ -26,7 +26,7 @@ public class SortUtils {
         int i = 0;
         int j = arr.length - 1;
         while (i < j) {
-            int middle = i + (j - i) / 2;  //防止middle的和超过Integer.MAX_VALUE的值
+            int middle = i + (j - i) / 2;   //防止middle的和超过Integer.MAX_VALUE的值
             if (key > arr[middle]) {
                 i = middle + 1;
             } else if (key < arr[middle]) {
