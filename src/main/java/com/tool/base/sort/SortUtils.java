656eb71d86ac774f189a0830a3a8142c9a5dbac1
octopus <345293721@qq.com>
2023-06-04 16:59:04 +0800

    Update SortUtils.java

--- src/main/java/com/tool/base/sort/SortUtils.java
+++ src/main/java/com/tool/base/sort/SortUtils.java
@@ -126,8 +126,9 @@ public class SortUtils {
      * @param arr 数组
      */
     public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
+        int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
                 }
