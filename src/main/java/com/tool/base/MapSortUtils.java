3112fbd7c04b64c6a9418e8a8c1f944b5edc7631
octopus <345293721@qq.com>
2024-01-23 15:19:46 +0800

    Update MapSortUtils.java

--- src/main/java/com/tool/base/MapSortUtils.java
+++ src/main/java/com/tool/base/MapSortUtils.java
@@ -3,7 +3,7 @@ package com.tool.base;
 import java.util.*;
 
 /**
- * map进行测试
+ * MapSortUtils
  *
  * @author zhangyu (zhangyuyu417@gmail.com)
  */
@@ -26,19 +26,9 @@ public class MapSortUtils {
      * @param map 输入map
      * @return 链表
      */
-    public List<Map.Entry<String, Integer>> getEntrysSortedByValue(Map<String, Integer> map) {
+    public List<Map.Entry<String, Integer>> getEntrySortedByValueList(Map<String, Integer> map) {
         List<Map.Entry<String, Integer>> entrys = new ArrayList(map.entrySet());
-        Collections.sort(entrys, new MyComparator());
+        Collections.sort(entrys, (a, b) -> (a.getValue() - b.getValue()));
         return entrys;
-    }
-
-    /**
-     * 比较器
-     */
-    class MyComparator implements Comparator<Map.Entry<String, Integer>> {
-        @Override
-        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-            return o1.getValue() - o2.getValue();
-        }
     }
 }
